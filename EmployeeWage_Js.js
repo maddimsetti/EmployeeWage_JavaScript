8485108b84e1131ba5215d8d3934a2c84f767d21
prudhvi <prudhvieethakota123@gmail.com>
2021-06-05 20:47:09 +0530

    Added: Refactored code to write function for daily working Hours

--- EmployeeWage_Js.js
+++ EmployeeWage_Js.js
@@ -1,41 +1,29 @@
 console.log("Welcome To EmployeeWage Program");
 
 /**
- * @description //UC1: To Check Employee is Absent or Presnt
- * param: Here empCheck Generates Random Value 0 Or 1
- *        if 0 - Employee Absent,  1 - Employee Presnt
+ * @description //UC3: Refactored code to write function for daily working Hours
+ * param: Here empCheck Generates Random Value 0 Or 1 Or 2
+ *        if 0 - Employee Absent,  1 - Employee Part_Time, 2 - Employee Full_Time
  **/
- const IS_ABSENT = 0
- let empCheck = Math.floor(Math.random() * 10) % 2;
- if (empCheck == IS_ABSENT) {
-     console.log("Employee is Absent");
- } else {
-     console.log("Employee is Present");
- }
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
 
- /**
- * @description UC2: To Calculate Daily Employee Wage Based on Part_Time or Full_Time Work
- * param: Here empCheck Generates random values 0, 1 or 2
- *        if 0 Employee Absent, 1 Employee Part_Time, 2 Employee Full_Time  
- **/
-  const IS_PART_TIME = 1;
-  const IS_FULL_TIME = 2;
-  const PART_TIME_HOURS = 4;
-  const FULL_TIME_HOURS = 8;
-  const WAGE_PER_HOUR = 20;
+function getWorkingHours(empCheck) {
+   switch (empCheck) {
+       case IS_PART_TIME:
+           return empHrs = PART_TIME_HOURS;
+       case IS_FULL_TIME:
+           return empHrs = FULL_TIME_HOURS;
+       default:
+           return empHrs = 0;                
+   }
+}
   
-  let empHrs = 0;
-  empCheck = Math.floor(Math.random() * 10) % 3;
-  switch (empCheck) {
-      case IS_PART_TIME: 
-              empHrs = PART_TIME_HOURS;
-              break;
-      case IS_FULL_TIME:
-              empHrs = FULL_TIME_HOURS;
-              break;
-      default:
-              empHrs = 0;                
-  }
-  
-  let empWage = empHrs * WAGE_PER_HOUR; 
-  console.log("Employee Wage: " +empWage);
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);   //calling the function 
+let empWage = empHrs * WAGE_PER_HOUR; 
+console.log("Hours: " +empHrs+ " Employee Wage: " +empWage);
