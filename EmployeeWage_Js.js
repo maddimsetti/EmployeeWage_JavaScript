a6ad6002ff7684ef1c0435bad006876a3e02e8b9
prudhvi <prudhvieethakota123@gmail.com>
2021-06-05 21:05:47 +0530

    Added: Calculating Wages for a Month Assuming 20 Working Days

--- EmployeeWage_Js.js
+++ EmployeeWage_Js.js
@@ -1,7 +1,7 @@
 console.log("Welcome To EmployeeWage Program");
 
 /**
- * @description //UC3: Refactored code to write function for daily working Hours
+ * @description //UC3: Calculating wages for a Month assuming 20 Working Days in a Month
  * param: Here empCheck Generates Random Value 0 Or 1 Or 2
  *        if 0 - Employee Absent,  1 - Employee Part_Time, 2 - Employee Full_Time
  **/
@@ -10,6 +10,7 @@ const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck) {
    switch (empCheck) {
@@ -22,8 +23,11 @@ function getWorkingHours(empCheck) {
    }
 }
   
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);   //calling the function 
-let empWage = empHrs * WAGE_PER_HOUR; 
-console.log("Hours: " +empHrs+ " Employee Wage: " +empWage);
+let totalEmpHrs = 0;
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    totalEmpHrs += getWorkingHours(empCheck);  //Calling the Function
+
+}
+let empWage = totalEmpHrs * WAGE_PER_HOUR; 
+console.log("Hours: " +totalEmpHrs+ " Employee Wage: " +empWage);
