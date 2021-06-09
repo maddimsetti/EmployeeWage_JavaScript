9db10b0940f284e960ee5005b7861fd8650340d5
prudhvi <prudhvieethakota123@gmail.com>
2021-06-10 02:16:09 +0530

    Added: Code for EmployeePayRoll ObjectOperations using Arrow-Functions

--- EmployeeWage_Js.js
+++ EmployeeWage_Js.js
@@ -50,3 +50,24 @@ while(totalEmpHrs < MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS
 
 //UC10 - Objects
 console.log("Showing Daily Hours and Wage Earned = " +empDailyHrsAndWageArr);
+
+//UC11 - Using Object Functions along with Arrow Functions
+let totalWages = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+                                      .reduce ((totalWage, dailyHrsAndWage) => totalWage +=dailyHrsAndWage.dailyWage,0);
+let totalHours = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyWage > 0)
+                                      .reduce ((totalHours, dailyHrsAndWage) => totalHours +=dailyHrsAndWage.dailyHours,0);
+
+console.log("UC - 11A Total Hours:  " +totalHours+ " Total Wages: "  +totalWages); 
+
+process.stdout.write("UC11B Logging full work Days")
+empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 8)
+                     .forEach(dailyHrsAndWage => process.stdout.write(dailyHrsAndWage.toString()));
+                    
+let partWorkingDayStrArr = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage == 4)
+                                                 .map(dailyHrsAndWage => dailyHrsAndWage.toString());
+console.log("\nUC 11C - PartWorking Days Strings: " +partWorkingDayStrArr);
+
+let nonWorkingDayNums = empDailyHrsAndWageArr.filter(dailyHrsAndWage => dailyHrsAndWage.dailyHours == 0)
+                                              .map(dailyHrsAndWage => dailyHrsAndWage.dayNum);
+
+console.log("UC - 11D NonWorkingDayNums: " +nonWorkingDayNums);
